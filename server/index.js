7f3dedd99624089e6e562677a65e53054a09f075
Rômulo Henri <romulohenri000@gmail.com>
2024-03-10 14:09:31 -0300

    add: Endpoints

--- server/index.js
+++ server/index.js
@@ -9,11 +9,16 @@ app.use(express.json());
 // Root Get Request
 app.get('/', (req, res) => {
   console.log('SERVER: Root Request!');
-  res.send('hello world!');
+
+  if (password == '') {
+    res.json({active: false});
+    return;
+  }
+  res.json({active: true});
 });
 
-// Post Request
-app.post('/api/products', async (req, res) => {
+// Post Product Request
+app.post('/api/product', async (req, res) => {
   try {
     console.log('SERVER: Post Request!');
 
@@ -25,18 +30,55 @@ app.post('/api/products', async (req, res) => {
   }
 });
 
+// Get Products Request
+app.get('/api/products', async (req, res) => {
+  try {
+    console.log('SERVER: Get Request!');
+
+    const products = await Product.find({});
+    res.status(200).send(products);
+
+  } catch (error) {
+    res.status(500).send({message: error.message});
+  }
+});
+
+// Get Product(id) Request
+app.get('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(`SERVER: Get Request for id: ${id}`);
+
+    const product = await Product.findById(id);
+    if (!product) {
+      res.status(404).send({message: 'Product not found!'});
+      return;
+    }
+    res.status(200).send(product);
+
+  } catch (error) {
+    res.status(500).send({message: error.message});
+  }
+});
+
 // Start Server
 app.listen(3000, () => {
+  if (password == '') {
+    console.error('SERVER: Issert MongoDB password!');
+    return;
+  }
   console.log('SERVER: Server is running on port 3000!')
 });
 
-let password = 'W64Q84gxwdQdVGAX';
+let password = '';
 let uri = `mongodb+srv://romulohenri000:${password}@backenddb.fwhrjwq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`;
 
-mongoose.connect(uri)
+if (password) {
+  mongoose.connect(uri)
   .then(() => {
     console.log('SERVER: Connected to MongoDB!');
   })
   .catch((err) => {
-    console.error('SERVER: Could not connect to MongoDB!', err);
+    console.error('SERVER: Could not connect to MongoDB!', err.message);
   });
+}
